611e271394da4692d5e13e6e5a1a1416fd4e62e8
Nikhil-75 <nikhilpandey4591@gmail.com>
2023-03-22 19:13:13 +0530

    forget password verfication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,72 @@
 const { UserData } = require('../models/userdata')
 const key = require("../config");
 const jwtService = require('../services/jwtService')
-
+const { v4: uuidv4 } = require('uuid');
+const PasswordReset = require('../models/PasswordReset')
 const JWT_REFRESH = require('../config/index')
 const jwt = require('jsonwebtoken');
 const e = require('express');
 
 
 
-
 exports.forgetPassword = async (req, res, next) => {
   try {
-    const exist = await UserData.exists({ email: req.body.email });
-   console.log(exist)
-    const token = jwt.sign({ user_id: exist._id } , JWT_SECRET, "2h",
-    );
+    const { email } = req.body;
+    const exist = await UserData.exists({ email });
+    if (!exist)
+      res.status(400).json({ messase: error.message })
 
-    
+    const code = uuidv4();
+
+    await PasswordReset.deleteMany({ email });
+
+    var results = { email, code }
+    PasswordReset.create(results)
+
+    res.status(200).json({ message: "Reset code is success" })
+
+
+
+
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+exports.codeVerify = async (req, res, next) => {
+  try {
+
+    const { code, email } = req.body;
+    const user = await UserData.findOne({ email })
+    if (!user) {
+
+      return next({ message: "user not found" })
+    }
+    const userExist = await PasswordReset.findOne({ email })
+
+    if (code == userExist) {
+      return next({ message: "invalid user" })
+    }
+    const token = jwtService.sign({ _id: user._id, });
 
     res.status(200).json({ access_token: token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+
+
+exports.resetPassword = async (req, res) => {
+  try {
+    const Id = req.token;
+    const password = req.body.password;
+
+    const user = await UserData.findByIdAndUpdate(Id, password);
+    res.status(200).json({
+      message: "password updated successfully",
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -26,22 +74,6 @@ exports.forgetPassword = async (req, res, next) => {
 
 
 
-
-
-exports.resetPassword = async (req, res) => {
-  const Id =  req.token.user_id;
-  const password = req.body.password;
-  
-   try {
-    const user = await UserData.findByIdAndUpdate(Id, password, {new: true })
-    res.status(200).json({
-      message: "password update successfully",
-      access_token: user,
-    })
-   } catch (error) {
-    res.status(400).json({message:error.mesage})
-   }
-}
 
 
 
@@ -67,13 +99,13 @@ exports.userId = async (req, res) => {
 exports.userData = async (req, res) => {
   let access_token;
   let refresh_token;
-  
+
   try {
     const user = new UserData(req.body);
     const result = await user.save();
     //Token
-    const token = jwtService.sign({_id: result._id,})
-    const refresh_token = jwtService.sign({ _id: result._id, }, JWT_REFRESH, '1y' )
+    const token = jwtService.sign({ _id: result._id, })
+    const refresh_token = jwtService.sign({ _id: result._id, }, JWT_REFRESH, '1y')
     return res
       .status(200)
       .json({
---
+++ models/PasswordReset.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const {Schema}  = mongoose;
+const PasswordResetSchema = new mongoose.Schema({
+  code: { type: String, unique: true },
+
+  email: { type: String, unique: true, required: true },
+ isUsed: { type: Boolean,default:false},
+
+},
+{timestamp : true},
+);
+
+const PasswordReset = mongoose.model('forgetPassword', PasswordResetSchema);
+
+module.exports = PasswordReset
