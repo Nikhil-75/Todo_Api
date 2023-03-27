dcafdc3863a53f5d0d5ad904c0c049b5c963e014
Nikhil-75 <nikhilpandey4591@gmail.com>
2023-03-27 19:16:54 +0530

    working on crud operation of todo list

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,77 +1,105 @@
-const { Todo } =  require('../models/Todo');
-const { UserData } = require('../models/userdata');
+const { Todo } = require("../models/Todo");
+const { UserData } = require("../models/userdata");
 
 exports.addNewTodo = async (req, res) => {
-    const Id =  req.token.user_id;
-    try {
-      const user = UserData.findById(Id);
-      if(!user) {
-        res.status(400).json({message:"user not found"});
-      }
-      const  updatedData = {Id, ...req.body };
-      const todo = new Todo(updatedData);
-      const saveedData = await todo.save();
-      return res.status(200).json({message: "item added successfully",
-    saveedData: {todo_Id: saveedData.id},})
-    } catch (error) {
-      res.status(400).json({message: error.message});
-    }
-  };
-
-
-  exports.getallTodo =  async (req,res) => {
-    const user_Id = req.token.user_id;
-    try {
-      const user = await UserData.findById(user_Id);
-      if (!user) {
-        res.status(400).json({ message: "user not found" });
-      }
-      const todos = await Todo.find({ user: req.user._id });
-      res.status(200).json(todos);
-    } catch (error) {
-      res.status(400).json({ message: 'No items!' });
-    }
-  }
-
-  exports.deleteTodo = async (req, res) => {
-    try {
-      const user_id = req.token.user_id;
-      const user = await UserData.findById(user_id);
-      if (!user)
-     res.status(400).json({ message: "user not found" });
-  
-      const Id = req.body._id;
-      const deleteTodo = await Todo.findByIdAndDelete(Id);
-      return res.status(200).json({ message: "todo deleted successfully. ",
-      deletedTodo: deleteTodo._id,});
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  };
-
-
-  
-exports.completeTodo = async (req, res) => {
-   try {
-    const user = await UserData.findById(req.token.user_id);
+  const Id = req.token._id;
+  try {
+    const user = UserData.findById(Id);
+    console.log(user);
     if (!user) {
       res.status(400).json({ message: "user not found" });
     }
-
-  const todo = await Todo.findById(_id);
-
-  if (todo) {
-    todo.done = true;
-    const updatedTodo = await todo.save();
-    if (updatedTodo) {
-      res.status(200).json({message: "updated Todo successfull "});
-    }
+    const updatedData = { Id, ...req.body, user_id: Id };
+    const todo = new Todo(updatedData);
+    const saveedData = await todo.save();
+    return res.status(200).json({
+      message: "item added successfully",
+      saveedData: { todo_Id: saveedData._id },
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-   } catch (error) {
-    
-    res.status(400).json({ message: "item not found" })
-   }
-}
+};
+
+exports.getallTodo = async (req, res) => {
+  const Id = req.token._id;
+  console.log(Id);
+  try {
+    const user = await UserData.findById(Id);
+    console.log(user);
+
+    if (!user) {
+      throw new Error({ message: "user not found" });
+    }
+    const todo = await Todo.find({ user_id  : Id });
+    console.log(todo);
+    res.status(200).json({ todo });
+  } catch (error) {
+    res.status(400).json({ message: "no item!" });
+  }
+};
 
 
-  
+exports.completeTodo = async (req, res) => {
+  const Id = req.token._id;
+  try {
+    const user = await UserData.findById(Id);
+
+    if (!user) {
+      throw new Error({ message: "user not found" });
+    }
+    const todo = await Todo.findById({ _id :req.body._id} );
+    console.log(todo,'==<<');
+
+
+    if (todo) {
+      todo.done = true;
+      const updatedTodo = await todo.save();
+
+      console.log(updatedTodo)
+      if (updatedTodo) {
+        res.status(200).json({ message: "updated Todo successfull " });
+      }
+    }
+  } catch (error) {
+    //console.log(error)
+    res.status(400).json({ message: "item not found" });
+  }
+};
+
+/*exports.showTodo = async (req, res) => {
+
+
+  try {
+    const showTodo = await Todo.findById(_id);
+   console.log(showTodo)
+    return res.status(200).json({  message:   { showTodo } });
+  } catch (error) {
+    res.status(400).json({ message: "user not show" });
+  }
+};*/
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    //const user_id = req.token.user_id;
+    //let Id = req.token._id;
+   // const user = await UserData.findById(user_id);
+
+    const user =  await UserData.findById(req.token._id)
+    if (!user)
+    throw new Error({ message: "user not found" });
+
+    const Id = req.body._id;
+    const deleteTodo = await Todo.findByIdAndDelete(Id);
+    return res.status(200).json({
+      message: "todo deleted successfully. ",
+      deletedTodo: deleteTodo._id,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+
+
